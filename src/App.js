e5adca966d71a34f59bc21847ced31979e7d1cb4
Rushvi Patel <72409300+rushvipatel@users.noreply.github.com>
2025-04-10 20:27:22 -0400

    deleted console.log

--- src/App.js
+++ src/App.js
@@ -37,7 +37,6 @@ function App() {
       const res = await axios.get("http://localhost:4000/profile", {
         headers: { Authorization: `Bearer ${token}` },
       });
-      console.log(res.data);
       setProfile(res.data);
     } catch (err) {
       alert("Failed to fetch profile");
